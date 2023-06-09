9843debc12abed8414bf2f536931c4435dfd0034
unknown <chinmaymaldar2002@gmail.com>
2023-06-09 23:05:50 +0530

    Final commit

--- src/TicTacToe/Tictactoe.js
+++ src/TicTacToe/Tictactoe.js
@@ -1,5 +1,5 @@
-import React,{useState} from 'react'
-import "./Tictactoe.css"
+import React,{useState} from 'react';
+import "./Tictactoe.css";
 const Tictactoe = () => {
 
 const [turn,setTurn]=useState('X');
